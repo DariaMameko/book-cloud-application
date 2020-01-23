1d64983dc252c8e6d361aa4da81564d4924212bb
Daria Mameko <Daria_Mameko@epam.com>
2020-01-23 18:38:38 +0200

    add styles and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BookDetailsComponent } from './books/book-details/book-details.component';
 import { BookListComponent } from './books/book-list/book-list.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'books',
+    component: BookListComponent,
+  },
+  { path: '',
+    redirectTo: '/books',
+    pathMatch: 'full'
+  },
+  { path: 'about', component: AboutComponent }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     BookDetailsComponent,
-    BookListComponent
+    BookListComponent,
+    MenuComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
